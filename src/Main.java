50e28a05f161a234eb24a137cd17d440004223fc
Danny Wong <wkydanny@users.noreply.github.com>
2026-02-23 15:48:17 -0500

    update DannyCode to DannyString in Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import varaibles.StringExample;
-
 public class Main {
-
-    //everything inside belongs to the Main class
-
     public static void main(String[] args) {
-        //everything inside belongs to the main method
-        StringExample.show();
-}
+
+        //DannyCode.process();
+
+        DannyVariable.main(new String[0]);
+
+    }
 }
